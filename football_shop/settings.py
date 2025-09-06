18532f0a9f2dae7828de824406325001d5c0e53a
kanayradeeva010 <adeevakanayra@gmail.com>
2025-09-06 23:17:55 +0700

    Merubah nama brand karena ternyata tidak bisa pake capslock

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kanayra-maritza-ADVKickHub.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kanayra-maritza-kavza.pbp.cs.ui.ac.id"]
 
 
 # Application definition
