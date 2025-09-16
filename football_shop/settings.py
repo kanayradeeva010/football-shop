7ee0445ceae4bff62f81275ebfb8c69c9f84812e
kanayradeeva010 <adeevakanayra@gmail.com>
2025-09-16 12:44:20 +0700

    Merubah sedikit details di link https pada settings.py

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kanayra-maritza-kavza.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://kanayra-maritza-kavza.pbp.cs.ui.ac.id/"
+    "https://kanayra-maritza-kavza.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
